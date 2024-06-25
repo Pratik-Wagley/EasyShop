de3d74649e8ce7f585973fd93bd413bd8c11d83e
Pratik-Wagley <pratikwagleywork@gmail.com>
2024-06-25 14:18:36 -0400

    Created the getById method in the controller

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -42,11 +42,13 @@ public class CategoriesController
 
     // add the appropriate annotation for a get action
 
+    @RequestMapping(path = "/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
+
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
